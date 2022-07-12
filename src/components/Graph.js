c795a411f48c9929b15d5113325a4371994c0998
Yousae <yousaex24@gmail.com>
2022-07-12 00:36:29 -0500

    working on animation

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { nanoid } from 'nanoid';
 
 const Graph = () => {
 
     const createArray = () => {
         const array = []
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 10; i++) {
             array.push(Math.floor(Math.random() * 400) + 1)
         }
         return array;
@@ -22,68 +22,90 @@ const Graph = () => {
         return graphArray;
     }
 
-    const [bars, setBars] = useState(createBars())
-
     // function to generate a new array and render new graph with "Generate New Array" button is clicked
     const generateNewArray = () => {
         setNums(createArray())
-        setBars(createBars())
     }
 
-    const bubbleSort = () => {
-        let animationCount = 1;
-        let barsCopy = []
+    // NEED TO DO
+    // have className for the bars - all the same
+    // have an animation array that stores an array of 2 values that are being compared
+    // loop through the animation array
+    // grab all the bars with byClassName -> returns a array of bars
+    // setTimeout to change the 2 bar color
+    // get the two bars via index from the animation array
+    // settimeout and set the bar height
 
-        // get and create object of only the id and height of each bar from state variable bars
-        for (let bar of bars) {
-            let barObj = {
-                id: bar.props.id,
-                height: bar.props.style.height
-            }
-            barsCopy.push(barObj);
-        }
-
-        for (let i = 0; i < barsCopy.length - 1; i++) {
-            for (let j = 0; j < barsCopy.length - i - 1; j++) {
-
-                const elem1Height = parseInt(barsCopy[j].height, 10);
-                const elem2Height = parseInt( barsCopy[j + 1].height, 10);
-
-                if (elem1Height > elem2Height) {
-
-                    // Change height in barsCopy
-                    const b1 = barsCopy[j + 1].height
-                    const temp = barsCopy[j].height;
-                    barsCopy[j].height = barsCopy[j + 1].height;
-                    barsCopy[j + 1].height = temp;
-
-                    setTimeout(() => {
-                        // Make changes to the DOM
-                        document.getElementById(barsCopy[j].id).style.height = b1
-                        document.getElementById(barsCopy[j + 1].id).style.height = temp
-                        console.log("time out")
-                    },animationCount * 50)
-
-                    
-
-                    animationCount++;
+    const sort = () => {
+        let animationCount = 0;
+        let array = [...nums]
+        for (let i = 0; i < 50; i++) {
+            for (let j = 0; j < 50 - 1 - i; j++) {                    
+                if (array[j] > array[j + 1]) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    setNums(() => {
+                        setTimeout(() => {
+                            console.log(array)
+                        }, 1000)
+                        return array
+                    })
+                    animationCount++
                 }
-
             }
         }
-        // console.log('\n')
-        // for (let bar of barsCopy) {
-        //     console.log(bar)
-        // }
     }
+    
+
+    // const bubbleSort = () => {
+    //     let animationCount = 1;
+    //     let barsCopy = []
+
+    //     // get and create object of only the id and height of each bar from state variable bars
+    //     for (let bar of bars) {
+    //         let barObj = {
+    //             id: bar.props.id,
+    //             height: bar.props.style.height
+    //         }
+    //         barsCopy.push(barObj);
+    //     }
+
+    //     // Sort the bars
+    //     for (let i = 0; i < barsCopy.length - 1; i++) {
+    //         for (let j = 0; j < barsCopy.length - i - 1; j++) {
+
+    //             const elem1Height = parseInt(barsCopy[j].height, 10);
+    //             const elem2Height = parseInt( barsCopy[j + 1].height, 10);
+
+    //             if (elem1Height > elem2Height) {
+
+    //                 // Change height in barsCopy
+    //                 const b1 = barsCopy[j + 1].height
+    //                 const temp = barsCopy[j].height;
+    //                 barsCopy[j].height = barsCopy[j + 1].height;
+    //                 barsCopy[j + 1].height = temp;
+
+    //                 setTimeout(() => {
+    //                     // Make changes to the DOM
+    //                     document.getElementById(barsCopy[j].id).style.height = b1
+    //                     document.getElementById(barsCopy[j + 1].id).style.height = temp
+    //                 }, animationCount * 25)
+
+    //                 animationCount++;
+    //             }
+    //         }
+    //     }
+        
+    // }
 
     return (
         <div>
             <div className='graph' style={{display: 'grid', gridTemplateColumns: 'repeat(' + nums.length + ', 10px)'}}>
-                {bars}
+                {createBars()}
             </div>
             <button onClick={generateNewArray}>Generate New Array</button>
-            <button onClick={bubbleSort}>Bubble Sort</button>
+            <button onClick={sort}>Bubble Sort</button>
         </div>
     )
 }
